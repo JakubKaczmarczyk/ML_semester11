ce59e696d34a462b6f2b14cd0b49d3bb53439ce6
Jakub Kaczmarczyk <jakubkaczmarczyk103@gmail.com>
2024-11-14 19:53:17 +0100

    find opt theta using fminunc from optimization toolbox

--- lab2/FindTheta.m
+++ lab2/FindTheta.m
@@ -1,21 +1,5 @@
 function [ThetaOpt,JOpt] = FindTheta(Theta0,X,Y)
-    alpha = 0.8;
-    n = 1000000;
-    Theta = Theta0;
-    i = 0;
-    mindJ = 0.0001;
-    while i < n
-        [J,dJ]=CostFun(X,Y,Theta);
-        if norm(dJ) < mindJ
-            break
-        end
-        Theta = Theta - alpha*dJ;
-        i = i+1;
-    end
-    ThetaOpt = Theta;
-    JOpt = J;
-    global iterations;
-    iterations = i;
-    % disp(i)
-    % disp(JOpt)
+    [x,fval,exitflag,output,grad,hessian] = fminunc(@(t) CostFun(X,Y,t), Theta0, optimoptions('fminunc', 'GradObj', 'on'));
+    ThetaOpt = x;
+    JOpt = fval;
 end
--- lab2/RunML2.m
+++ lab2/RunML2.m
@@ -27,12 +27,12 @@ Theta0 = rand(8, 1);
 % znalezienie optymalnego wektora Theta
 [ThetaOpt01,JOpt01] = FindTheta(Theta0,X_ready,Y1);
 [ThetaOpt02,JOpt02] = FindTheta(Theta0,X_ready,Y2);
-% [ThetaOpt1,JOpt1] = FindTheta(Theta0,X1tr,Y1tr);
-% [ThetaOpt2,JOpt2] = FindTheta(Theta0,X2tr,Y2tr);
+[ThetaOpt1,JOpt1] = FindTheta(Theta0,X1tr,Y1tr);
+[ThetaOpt2,JOpt2] = FindTheta(Theta0,X2tr,Y2tr);
 % 
-% % sprawdzenie zbioru testowego
-% [J1_val,dJ1_val] = CostFun(X1val,Y1val,ThetaOpt1);
-% [J2_val,dJ2_val] = CostFun(X2val,Y2val,ThetaOpt2);
+% sprawdzenie zbioru testowego
+[J1_val,dJ1_val] = CostFun(X1val,Y1val,ThetaOpt1);
+[J2_val,dJ2_val] = CostFun(X2val,Y2val,ThetaOpt2);
 
 set(gcf, 'WindowState', 'maximized');
 subplot(2, 2, 1);
@@ -49,27 +49,20 @@ PlotData(X,Yh2);
 
 % Wypisanie hipotezy
 imie_nazwisko = "Jakub Kaczmarczyk";
-max_iterations = 1000000;
 lambda = 0.01;
 alpha = 0.1;
 train_percent = 70;
 test_percent = 30;
 validation_percent = 0;
-global iterations;
-
-params_text = sprintf(['Jakub Kaczmarczyk' newline ...
-                       'Maks liczba iteracji: %d' newline ...
-                       'Uczenie zakończone po %d iteracjach' newline ...
-                       'Wielkość kroku uczenia alfa: %.1f' newline ...
+params_text1 = sprintf(['Jakub Kaczmarczyk' newline ...
                        'Początkowy wektora Theta uzupełniony wartościami losowymi' newline ...
                        'Podział zbioru: uczący %d%%, testowy %d%%, walidacyjny %d%%' newline ...
                        'Uzyskany błąd na zbiorze treningowym Hipotezy 1: %.4f' newline ...
                        'Uzyskany błąd na zbiorze treningowym Hipotezy 2: %.4f' newline ...
                        'Uzyskany błąd na zbiorze testowym Hipotezy 1: %.4f' newline ...
                        'Uzyskany błąd na zbiorze testowym Hipotezy 2: %.4f'], ...
-                       max_iterations, iterations, alpha, ...
-                       train_percent, test_percent, validation_percent, JOpt01, JOpt01, JOpt02, JOpt02);
-disp(params_text);
+                       train_percent, test_percent, validation_percent, JOpt1, JOpt2, J1_val, J2_val);
+disp(params_text1);
 
 
 disp('Confusion Matrix for Hypothesis 1:');
